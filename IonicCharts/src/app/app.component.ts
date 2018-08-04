797f00375136cf9509f51b83a146a71928bb88cf
Paweł Bagadziński <pbagadzinski@outlook.com>
2018-08-03 23:26:53 -0400

    Initial commit.

--- IonicCharts/src/app/app.component.ts
+++ IonicCharts/src/app/app.component.ts
@@ -21,8 +21,8 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
+      { title: 'Weekly Budget', component: Page1 },
+      { title: 'About this App', component: Page2 }
     ];
 
   }
--- IonicCharts/src/pages/page1/page1.ts
+++ IonicCharts/src/pages/page1/page1.ts
@@ -2,16 +2,57 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+declare var google: any;
+
 @Component({
-    selector: 'page-page1',
-    templateUrl: 'page1.html'
+  selector: 'page-page1',
+  templateUrl: 'page1.html'
 })
-export class Page1 {
 
-    constructor(public navCtrl: NavController) {
-    }
+export class Page1
+{
 
-    onLink(url: string) {
-        window.open(url);
-    }
+  constructor(public navCtrl: NavController)
+  {
+    this.initChart();
+  }
+
+  private initChart()
+  {
+    // Load the Visualization API and the corechart package.
+    google.charts.load('current', { 'packages': ['corechart'] });
+
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(this.drawChart);
+  }
+
+  // Callback that creates and populates a data table,
+  // instantiates the pie chart, passes in the data and
+  // draws it.
+  public drawChart()
+  {
+    let housing: number = parseInt((<HTMLInputElement>document.getElementById("housingVar")).value);
+    let commute: number = parseInt((<HTMLInputElement>document.getElementById('commuteVar')).value);
+    let grocery: number = parseInt((<HTMLInputElement>document.getElementById('groceryVar')).value);
+    let insurance: number = parseInt((<HTMLInputElement>document.getElementById('insuranceVar')).value);
+    let other: number = parseInt((<HTMLInputElement>document.getElementById('otherVar')).value);
+    let savings: number = parseInt((<HTMLInputElement>document.getElementById('savingsVar')).value);
+
+    var data = google.visualization.arrayToDataTable([
+      ['Expense', 'Cost per Week'],
+      ['Housing', housing],
+      ['Commute', commute],
+      ['Grocery', grocery],
+      ['Insurance', insurance],
+      ['Other', other],
+      ['Savings', savings]
+    ]);
+    var options = {
+      //title: 'Weekly Budget',
+      pieHole: 0.2
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+    chart.draw(data, options);
+  }
 }
